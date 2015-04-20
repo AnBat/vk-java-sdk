34bd95cf5d8021a4032e503907f5442057f95848
Anton Batiaev <a.batyaev@gmail.com>
2015-04-20 22:37:57 +0400

    describle attachment datatype

---
+++ vk_sdk/src/com/batyaev/vk/api/dataTypes/VKAttachmentType.java
@@ -0,0 +1,21 @@
+package com.batyaev.vk.api.dataTypes;
+
+/**
+ * Created by anton on 20/04/15.
+ * ---
+ * Copyright © 2015. Anton Batiaev. All Rights Reserved.
+ * www.batyaev.com
+ */
+enum VKAttachmentType {
+    Photo,
+    Posted_photo,
+    Video,
+    Audio,
+    Doc,
+    Graffiti,
+    Url,
+    Note,
+    App,
+    Poll,
+    Page
+}
