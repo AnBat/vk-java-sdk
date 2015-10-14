5408cf4f421d760f6206020f78a237f3f5cf7e86
Anton Batyaev <batyaev@haulmont.com>
2015-10-14 16:25:48 +0400

    fix gradle configs

---
+++ demo/src/TestApp.java
@@ -0,0 +1,56 @@
+import com.batiaev.vk.api.VKApi;
+import com.batiaev.vk.api.VKAuthorization;
+import com.batiaev.vk.api.VKParameters;
+import com.batiaev.vk.api.consts.VkApiMessagesParams;
+import com.batiaev.vk.api.dataTypes.VKMessage;
+import com.batiaev.vk.api.dataTypes.VKMessageList;
+import com.batiaev.vk.api.dataTypes.VKUserList;
+import com.batiaev.vk.sdk.VkLocalCache;
+import com.batiaev.vk.sdk.VkMessageCache;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author batiaev
+ * Created by batiaev on 28/06/15.
+ * ---
+ * Copyright © 2015. Anton Batiaev. All Rights Reserved.
+ * www.batiaev.com
+ */
+public class TestApp {
+    private static final Logger LOG = LogManager.getLogger(TestApp.class);
+
+    public static void main(String[] args) throws IOException {
+
+        VKAuthorization.loadProperties();
+        VkLocalCache.cachingFriends();
+//        authorization.serverAuth();
+//        authorization.clientAuth();
+
+        LOG.error("Friends requests count: " + VKApi.friends().getRequests(new VKParameters()).size());
+
+        VKParameters parameters = new VKParameters();
+        parameters.setValue(VkApiMessagesParams.COUNT, 20);
+        parameters.setValue(VkApiMessagesParams.USER_ID, "8475109");
+        VKMessageList messages = VKApi.messages().getHistory(parameters);
+
+        LOG.info("Total count of messages: " + messages.totalCount);
+        LOG.info("Count of unread messages: " + messages.upreadCount);
+        for (VKMessage message : messages) {
+            int userId = message.userId();
+            if (!VkLocalCache.hasUser(userId)) {
+                VKParameters userParams = new VKParameters();
+                userParams.setValue("userId", userId);
+                userParams.setValue("fields", "first_name, last_name");
+                VKUserList users = VKApi.users().get(userParams);
+
+                VkLocalCache.setUser(userId, users.get(0).fullName());
+            }
+            SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+            LOG.info(VkLocalCache.getUser(userId) + " " + dateFormat.format(message.getDate()) + " " + message.body());
+        }
+    }
+}
--- message_loader/src/MessageLoader.java
+++ message_loader/src/MessageLoader.java
@@ -6,24 +6,41 @@ import com.batiaev.vk.sdk.VkMessageCache;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 /**
  * @author batiaev
  * Created by batiaev on 10/14/15.
  */
-public class MessageLoader {
+public class MessageLoader extends TimerTask {
     private static final Logger LOG = LogManager.getLogger(MessageLoader.class);
+    private VKUserList friends;
+    private int currentIndex;
 
     public static void main(String[] args) {
 
+        MessageLoader loader = new MessageLoader();
+        Timer timer = new Timer();
+        timer.schedule(loader, 0, 10000); //checker each 10 sec
+    }
+
+    public MessageLoader() {
         VKAuthorization.loadProperties();
+
         VKParameters params = new VKParameters();
         params.setValue("user_id", VKAuthorization.userId());
         params.setValue("order", "hints");
         params.setValue("fields", "uid");
-        VKUserList friends = VKApi.friends().get(params);
-        friends.forEach(vkUser -> {
-            VkMessageCache messageCache = new VkMessageCache();
-            messageCache.cachingMassages(vkUser.userId());
-        });
+        friends = VKApi.friends().get(params);
+        currentIndex = 0;
+    }
+
+    @Override
+    public void run() {
+        LOG.info(String.format("Load messages for %s", friends.get(currentIndex).fullName()));
+        VkMessageCache messageCache = new VkMessageCache();
+        messageCache.cachingMassages(friends.get(currentIndex).userId());
+        currentIndex++;
     }
 }
